a645c798da195aaba6cc8f53494442582dc325d9
Fang Jin <fang.jin@pearson.com>
2023-10-19 14:12:11 -0400

    add example ball pan example

--- App.tsx
+++ App.tsx
@@ -1,5 +1,11 @@
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { Home } from "./Home";
+import Example from "./Example";
 
 export default function App() {
-  return <Home />;
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <Example />
+    </GestureHandlerRootView>
+  );
 }
